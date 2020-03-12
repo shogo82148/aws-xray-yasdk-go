249aa7029fe1dba31a2392874c4ac8919ef5fe68
Ichinose Shogo <shogo82148@gmail.com>
2020-03-12 13:29:35 +0900

    add testcase for connecting https server

--- xrayhttp/client_test.go
+++ xrayhttp/client_test.go
@@ -90,3 +90,67 @@ func TestClient(t *testing.T) {
 		t.Errorf("mismatch (-want +got):\n%s", diff)
 	}
 }
+
+func TestClient_TLS(t *testing.T) {
+	ctx, td := xray.NewTestDaemon(nil)
+	defer td.Close()
+
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		if _, err := w.Write([]byte("hello")); err != nil {
+			panic(err)
+		}
+	}))
+	defer ts.Close()
+
+	func() {
+		client := Client(ts.Client())
+		ctx, root := xray.BeginSegment(ctx, "test")
+		defer root.Close()
+		req, err := http.NewRequest(http.MethodGet, ts.URL, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req = req.WithContext(ctx)
+		req.Host = "example.com"
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal()
+		}
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal()
+		}
+		if string(data) != "hello" {
+			t.Errorf("want %q, got %q", "hello", string(data))
+		}
+	}()
+
+	got, err := td.Recv()
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := &schema.Segment{
+		Name: "test",
+		Subsegments: []*schema.Segment{
+			{
+				Name:      "example.com",
+				Namespace: "remote",
+				Subsegments: []*schema.Segment{
+					{
+						Name: "connect",
+						Subsegments: []*schema.Segment{
+							{Name: "dial"},
+							{Name: "tls"},
+						},
+					},
+					{Name: "request"},
+				},
+			},
+		},
+	}
+	if diff := cmp.Diff(want, got, ignoreVariableField); diff != "" {
+		t.Errorf("mismatch (-want +got):\n%s", diff)
+	}
+}
