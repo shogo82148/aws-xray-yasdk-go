81265b4089dfe91219c18e5d9ccf7c1456fa76f1
Ichinose Shogo <shogo82148@gmail.com>
2020-03-14 13:48:29 +0900

    naive implementation of tls.CipherSuiteName from Go 1.14

--- xrayhttp/tracer.go
+++ xrayhttp/tracer.go
@@ -162,17 +162,61 @@ func tlsVersionName(version uint16) string {
 	return fmt.Sprintf("0x%04X", version)
 }
 
-// copied from https://github.com/golang/go/blob/20a838ab94178c55bc4dc23ddc332fce8545a493/src/crypto/tls/cipher_suites.go#L97-L109
+// naive implementation of tls.CipherSuiteName from Go 1.14
 func cipherSuiteName(id uint16) string {
-	for _, c := range tls.CipherSuites() {
-		if c.ID == id {
-			return c.Name
-		}
-	}
-	for _, c := range tls.InsecureCipherSuites() {
-		if c.ID == id {
-			return c.Name
-		}
+	switch id {
+	case 0x0005:
+		return "TLS_RSA_WITH_RC4_128_SHA"
+	case 0x000a:
+		return "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
+	case 0x002f:
+		return "TLS_RSA_WITH_AES_128_CBC_SHA"
+	case 0x0035:
+		return "TLS_RSA_WITH_AES_256_CBC_SHA"
+	case 0x003c:
+		return "TLS_RSA_WITH_AES_128_CBC_SHA256"
+	case 0x009c:
+		return "TLS_RSA_WITH_AES_128_GCM_SHA256"
+	case 0x009d:
+		return "TLS_RSA_WITH_AES_256_GCM_SHA384"
+	case 0xc007:
+		return "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA"
+	case 0xc009:
+		return "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"
+	case 0xc00a:
+		return "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA"
+	case 0xc011:
+		return "TLS_ECDHE_RSA_WITH_RC4_128_SHA"
+	case 0xc012:
+		return "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA"
+	case 0xc013:
+		return "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA"
+	case 0xc014:
+		return "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
+	case 0xc023:
+		return "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
+	case 0xc027:
+		return "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
+	case 0xc02f:
+		return "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+	case 0xc02b:
+		return "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+	case 0xc030:
+		return "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+	case 0xc02c:
+		return "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+	case 0xcca8:
+		return "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+	case 0xcca9:
+		return "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
+	case 0x1301:
+		return "TLS_AES_128_GCM_SHA256"
+	case 0x1302:
+		return "TLS_AES_256_GCM_SHA384"
+	case 0x1303:
+		return "TLS_CHACHA20_POLY1305_SHA256"
+	case 0x5600:
+		return "TLS_FALLBACK_SCSV"
 	}
 	return fmt.Sprintf("0x%04X", id)
 }
