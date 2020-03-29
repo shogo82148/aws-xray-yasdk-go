aec4828173e712cecde28d9eac8bb111448889b7
Ichinose Shogo <shogo82148@gmail.com>
2020-03-29 11:27:32 +0900

    add benchmark for client

--- xray/client_test.go
+++ xray/client_test.go
@@ -2,8 +2,31 @@ package xray
 
 import (
 	"testing"
+
+	"github.com/shogo82148/aws-xray-yasdk-go/xray/schema"
 )
 
-func TestClientEmit(t *testing.T) {
+func BenchmarkClient(b *testing.B) {
+	ctx, td := NewNullDaemon()
+	defer td.Close()
+	client := ContextClient(ctx)
+	seg := &schema.Segment{
+		Name:      "foobar",
+		ID:        "03babb4ba280be51",
+		TraceID:   "1-5e645f3e-1dfad076a177c5ccc5de12f5",
+		StartTime: 1000000000,
+		EndTime:   1000000000,
+		Service:   ServiceData,
+		AWS: &schema.AWS{
+			XRay: &schema.XRay{
+				Version: Version,
+				Type:    Type,
+			},
+		},
+	}
 
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		client.emit(ctx, seg)
+	}
 }
