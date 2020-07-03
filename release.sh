989a73de6cb12b3d0464669ad4ca9b817ef34443
Ichinose Shogo <shogo82148@gmail.com>
2020-07-04 00:39:29 +0900

    add a script for release

---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -uex
+
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+VERSION=$1
+MAJOR=$(echo "$VERSION" | cut -d. -f1)
+MINOR=$(echo "$VERSION" | cut -d. -f2)
+PATCH=$(echo "$VERSION" | cut -d. -f3)
+
+# update metadata
+cat <<EOF > "$CURRENT/xray/version.go"
+package xray
+
+// Version records the current X-Ray Go SDK version.
+const Version = "$MAJOR.$MINOR.$PATCH"
+EOF
+git add "$CURRENT/xray/version.go"
+git commit -m "bump up v$MAJOR.$MINOR.$PATCH"
--- xray/metadata.go
+++ xray/metadata.go
@@ -6,19 +6,12 @@ import (
 	"github.com/shogo82148/aws-xray-yasdk-go/xray/schema"
 )
 
-// Version records the current X-Ray Go SDK version.
-const Version = "0.0.4"
-
 // Name records which X-Ray SDK customer uses.
 const Name = "X-Ray YA-SDK for Go"
 
 // ServiceData is the metadata of the user service.
 // It is used by all segments that X-Ray YA-SDK sends.
-var ServiceData *schema.Service
-
-func init() {
-	ServiceData = &schema.Service{
-		Compiler:        runtime.Compiler,
-		CompilerVersion: runtime.Version(),
-	}
+var ServiceData = &schema.Service{
+	Compiler:        runtime.Compiler,
+	CompilerVersion: runtime.Version(),
 }
