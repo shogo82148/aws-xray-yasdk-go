03f9305cbbb1998b8a5b4d932f6d7c49937e360c
Ichinose Shogo <shogo82148@gmail.com>
2021-03-28 19:09:53 +0900

    add test case for failing to dial

--- xrayaws-v2/client_test.go
+++ xrayaws-v2/client_test.go
@@ -1,10 +1,13 @@
 package xrayaws
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -129,6 +132,122 @@ func TestClient(t *testing.T) {
 						Status:        200,
 						ContentLength: 2,
 					},
+				},
+				AWS: schema.AWS{
+					"operation": "ListFunctions",
+					"region":    "fake-moon-1",
+					// TODO: fix me!
+					// "request_id": "",
+					// "retries":    0.0,
+				},
+			},
+		},
+		Service: xray.ServiceData,
+	}
+	if diff := cmp.Diff(want, got, ignoreVariableField); diff != "" {
+		t.Errorf("mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestClient_FailDial(t *testing.T) {
+	// setup dummy X-Ray daemon
+	ctx, td := xray.NewTestDaemon(nil)
+	defer td.Close()
+
+	var opt config.LoadOptions
+	WithXRay()(&opt)
+	cfg := aws.Config{
+		Region: "fake-moon-1",
+		EndpointResolver: aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+			return aws.Endpoint{
+				// we expected no Gopher daemon on this computer ʕ◔ϖ◔ʔ
+				URL:         "http://127.0.0.1:70",
+				SigningName: "lambda",
+			}, nil
+		}),
+		Retryer: func() aws.Retryer {
+			return aws.NopRetryer{}
+		},
+		APIOptions: opt.APIOptions,
+	}
+
+	// start testing
+	svc := lambda.NewFromConfig(cfg)
+	ctx, root := xray.BeginSegment(ctx, "Test")
+	_, err := svc.ListFunctions(ctx, &lambda.ListFunctionsInput{})
+	root.Close()
+	if err == nil {
+		t.Fatal("want error, but no error")
+	}
+	awsErr := err
+	var urlErr *url.Error
+	if !errors.As(awsErr, &urlErr) {
+		t.Fatal(err)
+	}
+
+	// check the segment
+	got, err := td.Recv()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := &schema.Segment{
+		Name: "Test",
+		Subsegments: []*schema.Segment{
+			{
+				Name:      "lambda",
+				Namespace: "aws",
+				Fault:     true,
+				Cause: &schema.Cause{
+					WorkingDirectory: wd,
+					Exceptions: []schema.Exception{
+						{
+							Message: awsErr.Error(),
+							Type:    fmt.Sprintf("%T", awsErr),
+						},
+					},
+				},
+				Subsegments: []*schema.Segment{
+					{Name: "marshal"},
+					{
+						Name:  "attempt",
+						Fault: true,
+						Subsegments: []*schema.Segment{
+							{
+								Name:  "connect",
+								Fault: true,
+								Subsegments: []*schema.Segment{
+									{
+										Name:  "dial",
+										Fault: true,
+										Cause: &schema.Cause{
+											WorkingDirectory: wd,
+											Exceptions: []schema.Exception{
+												{
+													Message: urlErr.Err.Error(),
+													Type:    fmt.Sprintf("%T", urlErr.Err),
+												},
+											},
+										},
+										Metadata: map[string]interface{}{
+											"http": map[string]interface{}{
+												"dial": map[string]interface{}{
+													"network": "tcp",
+													"address": "127.0.0.1:70",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				HTTP: &schema.HTTP{
+					Response: &schema.HTTPResponse{},
 				},
 				AWS: schema.AWS{
 					"operation": "ListFunctions",
