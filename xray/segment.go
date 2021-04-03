ec0b6d9ab276190c62753cf0eb5dad7cd0811abe
Ichinose Shogo <shogo82148@gmail.com>
2021-04-04 06:58:36 +0900

    improve detaching a context

--- xray/segment.go
+++ xray/segment.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"reflect"
 	"strings"
 	"sync"
 	"time"
@@ -743,8 +744,39 @@ func AddAnnotationFloat64(ctx context.Context, key string, value float64) {
 }
 
 // DetachContextSegment returns a new context with the existing segment.
+// All values associated with ctx are also associated with the new context.
 // This is useful for creating background tasks which won't be cancelled
 // when a request completes.
 func DetachContextSegment(ctx context.Context) context.Context {
-	return context.WithValue(context.Background(), segmentContextKey, ContextSegment(ctx))
+	return &detachedContext{Context: ctx}
+}
+
+type detachedContext struct {
+	context.Context
+}
+
+func (*detachedContext) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (*detachedContext) Done() <-chan struct{} {
+	return nil
+}
+
+func (*detachedContext) Err() error {
+	return nil
+}
+
+func (ctx *detachedContext) String() string {
+	return contextName(ctx.Context) + ".Detached"
+}
+
+func contextName(c context.Context) string {
+	type stringer interface {
+		String() string
+	}
+	if s, ok := c.(stringer); ok {
+		return s.String()
+	}
+	return reflect.TypeOf(c).String()
 }
