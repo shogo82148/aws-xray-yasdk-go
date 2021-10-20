c4bbb8d7691b4a4f426536c78a07e28b7243cad2
Ichinose Shogo <shogo82148@gmail.com>
2021-10-20 12:33:40 +0900

    improve the release script

--- release.sh
+++ release.sh
@@ -3,10 +3,11 @@
 set -uex
 
 CURRENT=$(cd "$(dirname "$0")" && pwd)
-VERSION=$1
+VERSION=${1#v}
 MAJOR=$(echo "$VERSION" | cut -d. -f1)
 MINOR=$(echo "$VERSION" | cut -d. -f2)
 PATCH=$(echo "$VERSION" | cut -d. -f3)
+git switch main
 
 # update metadata
 cat <<EOF > "$CURRENT/xray/version.go"
@@ -18,4 +19,18 @@ EOF
 git add "$CURRENT/xray/version.go"
 git commit -m "bump up v$MAJOR.$MINOR.$PATCH"
 git tag "v$MAJOR.$MINOR.$PATCH"
-git push --tags
+git push "v$MAJOR.$MINOR.$PATCH"
+
+# update xrayaws
+cd "$CURRENT/xrayaws"
+go get "github.com/shogo82148/aws-xray-yasdk-go@v$MAJOR.$MINOR.$PATCH"
+go mod tidy
+
+# update xrayaws-v2
+cd "$CURRENT/xrayaws-v2"
+go get "github.com/shogo82148/aws-xray-yasdk-go@v$MAJOR.$MINOR.$PATCH"
+go mod tidy
+
+git add .
+git commit -m "bump aws-xray-yasdk-go v$MAJOR.$MINOR.$PATCH"
+git push origin main
