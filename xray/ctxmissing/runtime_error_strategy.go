7c3bac593abe9d03955f24d2af586bcacdec16f7
Ichinose Shogo <shogo82148@gmail.com>
2022-03-27 20:41:12 +0900

    run go fmt with Go 1.18

--- xray/ctxmissing/runtime_error_strategy.go
+++ xray/ctxmissing/runtime_error_strategy.go
@@ -3,7 +3,7 @@ package ctxmissing
 import "context"
 
 // RuntimeErrorStrategy panics when the segment context is missing.
-type RuntimeErrorStrategy struct {}
+type RuntimeErrorStrategy struct{}
 
 // ContextMissing implements Strategy.
 func (*RuntimeErrorStrategy) ContextMissing(ctx context.Context, v interface{}) {
