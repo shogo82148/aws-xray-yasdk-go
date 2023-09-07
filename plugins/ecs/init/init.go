fc6e63e74eab62ee04b242e81430492a630b0906
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-07 14:24:54 +0900

    add a package comment for xray/plugins/ecs/init

--- plugins/ecs/init/init.go
+++ plugins/ecs/init/init.go
@@ -1,3 +1,12 @@
+// Package init installs the ECS plugin at init time.
+// To enable this plugin, please import the ecs/init package.
+//
+//	import _ "github.com/shogo82148/aws-xray-yasdk-go/xray/plugins/ecs/init"
+//
+// or if you want to load conditionally at runtime, use Init() function.
+//
+//	import _ "github.com/shogo82148/aws-xray-yasdk-go/xray/plugins/ecs"
+//	ecs.Init()
 package init
 
 import "github.com/shogo82148/aws-xray-yasdk-go/plugins/ecs"
